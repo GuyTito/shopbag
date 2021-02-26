9b2a83a9a014e14d177d157be283c1a711e78d5f
GuyTito <profkofi@gmail.com>
2021-02-26 09:21:07 +0000

    Update HomePage.vue

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,6 +1,31 @@
 <template>
   <div class="about">
-    <h1>About</h1>
-    <p>A simple app to track grocery items and prices as you're shopping.</p>
+    <img src="" alt="Big image of the app covering the entire width">
+    <p>A simple personal shopping assistant to track your grocery items and their prices as you shop. A todo list for shopping.</p>
+    <button>Demo</button>
+    <p>You probably put together a grocery list before heading to the store — whether it's in your head or on a piece of paper. But how often do you actually remember to bring that list with you so you can grocery shop faster and track corresponding prices with your budget for future use? Instead of wandering the market aimlessly wondering how much tomatoes you have left or struggling to remember each item on your list, switch up how you create a shopping list in the first place. Next time, use ShopBag web app on your phone as your personal grocery shopping assistant.</p>
+    <div>features</div>
+    <div>
+      <img src="" alt="image">
+      <p>Quickly jot down on the go.
+        ShopBag is simple and easy to use. In a hurry for shopping or to the market, you can quickly create grocery lists. Write all the items you need in one place, optionally with your budget. Imagine the wahala when you return from the market find out that you forgot to buy spices for the jollof.</p>
+    </div>
+    <div>
+      <img src="" alt="image">
+      <p>Easily track prices.
+        ShopBag helps you to track prices and quantities of the items you will buy, and have bought by providing spaces to enter your budget and the price you bought. ShopBag will automatically calculate the difference and totals for you without you taking out a calculator to crunch numbers like Professor Allotey.</p>
+    </div>
+    <div>
+      <img src="" alt="image">
+      <p>Effortlessly stay organized
+        Colors make it intuitive to organize things. Once you see colors, you will instantly know how to use them to manage your life. Don’t worry if you are not good at organizing things. Anyone can easily get organized with ShopBag.</p>
+    </div>
+    <div>
+      <img src="" alt="image">
+      <p>No boundaries at all
+        ShopBag is a web app so your lists are backed up to cloud storage. Once you keep your lists in ShopBag, they will always follow you, even if you switch to another device. You never have to worry about losing your shopping list.</p>
+    </div>
+    <button>Demo</button>
+    <div>footer</div>
   </div>
 </template>
