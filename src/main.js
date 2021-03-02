2827aa19f52f7dcefca3744168e7c37859c86c69
GuyTito <profkofi@gmail.com>
2021-03-02 20:25:54 +0000

    added js files
    added js files to make bootstrap work

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import "bootstrap/dist/css/bootstrap.min.css";
-import './assets/style/main.scss';
+import "/node_modules/@popperjs/core/dist/umd/popper.min.js"
+import "/node_modules/bootstrap/dist/js/bootstrap.min.js"
+import "bootstrap/dist/css/bootstrap.min.css"
+import './assets/style/main.scss'
 
 createApp(App).use(store).use(router).mount('#app')
