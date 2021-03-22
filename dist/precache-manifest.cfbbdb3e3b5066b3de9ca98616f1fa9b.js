2b43d96de3a7621cd4ad0f8e71d1dacdabc31799
GuyTito <profkofi@gmail.com>
2021-03-22 13:06:22 +0000

    added assets
    generated assets and configured for manifest.json

---
+++ dist/precache-manifest.cfbbdb3e3b5066b3de9ca98616f1fa9b.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea6126d4c557120304b5",
+    "url": "/css/about.1a85aea5.css"
+  },
+  {
+    "revision": "495530a8e24c38850d27",
+    "url": "/css/app.f028e1b2.css"
+  },
+  {
+    "revision": "42939d160add470af504",
+    "url": "/css/chunk-05c36658.b2ffbb2b.css"
+  },
+  {
+    "revision": "8c4855925ebbd5e09ff6",
+    "url": "/css/chunk-720ae2fe.3af25c54.css"
+  },
+  {
+    "revision": "c2c66c37f607bed13cfb",
+    "url": "/css/chunk-vendors.9b9fa459.css"
+  },
+  {
+    "revision": "95b5803ebc81f38fd2b9604935b483f1",
+    "url": "/img/add.95b5803e.svg"
+  },
+  {
+    "revision": "37033fe17ebb5f1eaf4f1aff3b25586a",
+    "url": "/img/bin.37033fe1.svg"
+  },
+  {
+    "revision": "83bc04d12e595f9d439528e2c475e55b",
+    "url": "/img/hero2.83bc04d1.svg"
+  },
+  {
+    "revision": "11b432b9dc13ff42e4205d15bd41038d",
+    "url": "/img/logo_shopbag.11b432b9.svg"
+  },
+  {
+    "revision": "14decbbe9be71809c7600e8a3b0c6458",
+    "url": "/img/offline.14decbbe.png"
+  },
+  {
+    "revision": "9c4e559432f751783fc94ab5a2ad7978",
+    "url": "/img/pencil.9c4e5594.svg"
+  },
+  {
+    "revision": "16247231d2f3e2e6a86554bf3a3932ad",
+    "url": "/img/shopbags.16247231.svg"
+  },
+  {
+    "revision": "b36013d82159a29aca645c722160adde",
+    "url": "/img/stock_shopbag_list.b36013d8.png"
+  },
+  {
+    "revision": "22a28c9b616f8d916797f1244f8fedcb",
+    "url": "/img/stock_shopbag_prices.22a28c9b.png"
+  },
+  {
+    "revision": "dfc62bd0d2cbc07eeadc889a44ff3ba5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea6126d4c557120304b5",
+    "url": "/js/about.e3683ddd.js"
+  },
+  {
+    "revision": "495530a8e24c38850d27",
+    "url": "/js/app.f5cbd82b.js"
+  },
+  {
+    "revision": "42939d160add470af504",
+    "url": "/js/chunk-05c36658.0528f9cd.js"
+  },
+  {
+    "revision": "11171139c309f46d7465",
+    "url": "/js/chunk-1a80fee3.8db63c6c.js"
+  },
+  {
+    "revision": "8c4855925ebbd5e09ff6",
+    "url": "/js/chunk-720ae2fe.e7391b0e.js"
+  },
+  {
+    "revision": "c2c66c37f607bed13cfb",
+    "url": "/js/chunk-vendors.5d3d2de2.js"
+  },
+  {
+    "revision": "bd2623f91a7cc8c695a70eaa353e3326",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "b7379e7962a22945eec2fd48a417ff0a",
+    "url": "/shopbag_logo.png"
+  }
+]);
