f5fc9ac4eea0d1e5583e0edbb8ad547e45678d2a
GuyTito <profkofi@gmail.com>
2021-03-22 11:58:53 +0000

    installed pwa

--- src/main.js
+++ src/main.js
@@ -6,5 +6,6 @@ import "/node_modules/@popperjs/core/dist/umd/popper.min.js"
 import "/node_modules/bootstrap/dist/js/bootstrap.min.js"
 import "bootstrap/dist/css/bootstrap.min.css"
 import './assets/style/main.scss'
+import './registerServiceWorker'
 
 createApp(App).use(store).use(router).mount('#app')
