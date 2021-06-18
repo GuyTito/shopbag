97c0062e279986a0cfa3fc4850ce66e4266b5b7f
GuyTito <profkofi@gmail.com>
2021-06-18 11:55:38 +0000

    total budget and expense
    Calculate total amount and show diff in budget and bought prices

--- src/views/BagPage.vue
+++ src/views/BagPage.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- list of items -->
-  <div class="container-fluid mt-4 page-head" v-if="bag">
+  <div class="container-fluid mt-4 mb-4 page-head" v-if="bag">
     <div class="d-flex justify-content-between mb-2">
       <h2 class="mx-2">{{ bag.bag_name }}</h2>
 
@@ -22,9 +22,21 @@
         <Item :item="item" :currency="bag.currency" @del-item="removeItem" />
         <hr class="bg-primary" />
       </div>
-      <div class="mt-4">
+
+      <div class="mt-4 mb-4">
         <h6>Notes</h6>
         {{ bag.comment }}
+      </div>
+
+      <div>
+        <div><strong>Total Budget: </strong>{{ bag.currency }}{{total_budget}}</div>
+        <div><strong>Total Expense: </strong>{{ bag.currency }}{{total_expense}}</div>
+        <div>
+          <strong>Balance: </strong> 
+          <span :class="[balance < 0 ? 'text-danger' : '']"> 
+            {{ bag.currency }}{{balance}}
+          </span>
+        </div>
       </div>
     </div>
   </div>
@@ -36,9 +48,31 @@ import Item from "../components/Item";
 export default {
   name: "BagPage",
   components: { Item },
+  data(){
+    return{
+      total_budget: 0,
+      total_expense: 0,
+      balance: 0
+    }
+  },
   computed: {
     bag() {
-      return this.$store.getters.getBag(this.$route.params.id);
+      let bag = this.$store.getters.getBag(this.$route.params.id)
+      let items = bag.items
+      
+      let total_budget = items.reduce(function(sum, item) { 
+        return sum + (parseInt(item.budget_price) * parseInt(item.quantity));
+      }, 0)
+      this.total_budget = total_budget
+
+      let total_expense = items.reduce(function(sum, item) { 
+        return sum + (parseInt(item.bought_price) * parseInt(item.quantity));
+      }, 0)
+      this.total_expense = total_expense
+
+      this.balance = total_budget - total_expense
+
+      return bag;
     },
   },
   created() {
@@ -50,7 +84,7 @@ export default {
         bagId: this.bag.bag_id,
         itemId: itemId,
       });
-    }
+    },
   },
 };
 </script>
